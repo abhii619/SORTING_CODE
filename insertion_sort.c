7d290520149af0381b8a38bc85f186334e0f7d2d
abhii619 <maneabhii619@gmail.com>
2020-07-08 15:56:40 +0530

    hello

---
+++ insertion_sort.c
@@ -0,0 +1,40 @@
+
+
+#include<stdio.h>
+#include <stdlib.h>
+#include<string.h>
+
+
+int main (void)
+{
+int *ptr=(int*)malloc(sizeof(int)*10);
+printf("enter values in the array");
+for(int i=0;i<10;i++)
+{
+	scanf("%d",&ptr[i]);
+}
+for(int i=1;i<10;++i)
+{
+       	int min=i;
+for(int k=i-1;k>=0;--k)
+{
+   if(ptr[k]>ptr[min])
+   {
+	   int temp=ptr[k];
+           ptr[k]=ptr[min];
+           ptr[min]=temp;
+	   min=k;
+   }
+}
+for(int i=0;i<10;i++)
+{
+	printf("%d\t",ptr[i]);
+}
+printf("\n");
+}
+for(int i=0;i<10;i++)
+{
+	printf("%d\t",*ptr++);
+}
+return 0;
+}
